971e31cf6490440568db9b1eb644a2f22fba3fc2
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2022-01-05 20:05:31 -0600

    Update to supported query and cmdlet calls

--- Get-AzureAppGatewayExpiringCertificates.ps1
+++ Get-AzureAppGatewayExpiringCertificates.ps1
@@ -6,9 +6,17 @@ param(
 $pageSize = 100
 $iteration = 0
 $searchParams = @{
-    Query = 'where type =~ "Microsoft.Network/applicationGateways" | project id, subscriptionId, subscriptionDisplayName, resourceGroup, name, sslCertificates = properties.sslCertificates | order by id'
+    Query = 'resources
+    | where type =~ "Microsoft.Network/applicationGateways"
+    | extend ssl = parse_json(properties.sslCertificates)
+    | join kind=inner (
+        resourcecontainers
+        | where type == "microsoft.resources/subscriptions"
+        | project subscriptionId, subscriptionName = name)
+        on subscriptionId
+    | project name, subscriptionId, subscriptionName, resourceGroup, ssl
+    | order by name'
     First = $pageSize
-    Include = 'displayNames'
 }
 
 $results = do {
@@ -21,16 +29,16 @@ $results = do {
 } while ($pageResults.Count -eq $pageSize)
 
 $90daysfromNow = (Get-Date).AddDays($ExpiresInDays)
-$results | % {
+$results | foreach-object {
     $record = $_
 
-    $record.sslCertificates | % {
+    $record.ssl | foreach-object {
         $sslCertRecord = $_
         $cert = [System.Security.Cryptography.X509Certificates.X509Certificate2]([System.Convert]::FromBase64String($_.properties.publicCertData.Substring(60,$_.properties.publicCertData.Length-60)))
         if ($cert.NotAfter -le $90daysfromNow) {
-            @{
+            [pscustomobject]@{
                 SubscriptionId = $record.subscriptionId
-                SubscriptionName = $record.subscriptionDisplayName
+                SubscriptionName = $record.subscriptionName
                 ResourceGroup = $record.resourceGroup
                 Name = $record.Name
                 Cert = $cert
